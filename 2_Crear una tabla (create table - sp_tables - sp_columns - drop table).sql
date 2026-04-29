400d58472141ed411be92a2de6e619f03d287ba4
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-04-29 17:12:26 -0500

    anadiendo mas info

--- 2_Crear una tabla (create table - sp_tables - sp_columns - drop table).sql
+++ 2_Crear una tabla (create table - sp_tables - sp_columns - drop table).sql
@@ -1,4 +1,11 @@
+--creando base de datos
 create database bdnuevo;
 
-
+--usando la base de datos
 use bdnuevo
+
+--creando la tabla
+create table usuarios (
+nombre varchar (30),
+clave varchar(10)
+);
