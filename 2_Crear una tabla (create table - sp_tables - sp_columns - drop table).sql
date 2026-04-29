75f616fde9835c6cbd1ae1614a96765a5cc2683d
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-04-29 17:37:20 -0500

    anadi mas info

--- 2_Crear una tabla (create table - sp_tables - sp_columns - drop table).sql
+++ 2_Crear una tabla (create table - sp_tables - sp_columns - drop table).sql
@@ -9,3 +9,26 @@ create table usuarios (
 nombre varchar (30),
 clave varchar(10)
 );
+
+--para ver la estructura de la tabla
+exec sp_columns usuarios;
+
+--para borrar la tabla
+drop table usuarios;
+
+
+--LOTE DE COMANDOS CODIGO
+if object_id('usuarios')is not null
+drop table usuarios;
+
+create table usuarios(
+nombre varchar(30),
+clave varchar(10)
+);
+--listar las tablas de la base de datos
+exec sp_tables @table_owner='dbo';
+exec sp_columns usuarios;
+
+drop table usuarios;
+
+exec sp_tables @table_owner='dbo';
